c4dbb3c9499780f94530b21d0b46f206ad272f59
Tomas Ferreras <hellotomasdev@gmail.com>
2025-06-16 00:44:31 -0300

    Initial commit

---
+++ packages/my-lib/webpack.config.js
@@ -0,0 +1,60 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
+import { CleanWebpackPlugin } from "clean-webpack-plugin";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const isProduction = process.env.NODE_ENV === "production";
+
+export default {
+  mode: isProduction ? "production" : "development",
+  devtool: isProduction ? "source-map" : "eval-source-map",
+  entry: path.join(__dirname, "src", "index.ts"),
+  target: "es2020",
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "index.js",
+    library: {
+      type: "module",
+    },
+    environment: {
+      module: true,
+    },
+    clean: true,
+  },
+  experiments: {
+    outputModule: true,
+  },
+  optimization: {
+    minimize: isProduction,
+    usedExports: true,
+    sideEffects: false,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(tsx?)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "swc-loader",
+          options: {
+            sourceMap: true,
+            jsc: {
+              parser: {
+                syntax: "typescript",
+                tsx: true,
+              },
+              target: "es2020",
+            },
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+  },
+  plugins: [new CleanWebpackPlugin(), new ForkTsCheckerWebpackPlugin()],
+};
