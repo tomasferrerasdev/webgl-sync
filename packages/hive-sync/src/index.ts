ea07d0ecc648d0643d0167b9aa608a3f0e20f074
tomasfdev <tomas@basement.studio>
2025-06-17 15:53:08 -0300

    Remove Greeting component and its export from hive-sync package to streamline codebase.

--- packages/hive-sync/src/index.ts
+++ packages/hive-sync/src/index.ts
@@ -1,4 +1,3 @@
-export { default as Greeting } from "./components/Greeting";
 export { Hive } from "./components/hive/hive";
 export { Bee } from "./components/hive/hive";
 export { calculatePlaneDimensions } from "./components/hive/hive-utils";
